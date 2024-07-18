dafa970e28778b11aa229721d61b8a56e932dc3f
SamirAlmeidaCodes <samircalmeida7@gmail.com>
2024-07-18 15:32:49 +0100

    frontend-changes

--- noter/src/App.js
+++ noter/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import NoteApp from "./NoteApp";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Noter is under development. Message me for more details 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NoteApp />
     </div>
   );
 }
